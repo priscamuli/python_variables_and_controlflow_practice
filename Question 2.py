dede484fb8e305a6ba3b68d232e83a912311ba28
priscamuli <priscakamanthe31@gmail.com>
2024-10-23 19:39:49 +0300

    added odd or even checker

---
+++ Question 2.py
@@ -0,0 +1,10 @@
+#odd or even checker
+
+#ask the user fo a numer
+number=int(input("Enter a number: "))
+
+#check the number
+if number%2==0:
+    print("Even number")
+else:
+    print("Odd number")
