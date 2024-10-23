360bd6d642e60c27a2e1173e05bd371c058560b0
priscamuli <priscakamanthe31@gmail.com>
2024-10-23 19:17:58 +0300

    added variable assignment and arithmetic operation

---
+++ Question 1.py
@@ -0,0 +1,11 @@
+#variable assignment and arithmetic operation
+a=15
+b=10
+#addition of the two numbers
+print(a+b)
+#subtration of the two numbers
+print(a-b)
+#multplication of the two numbers
+print(a*b)
+#division of the two numbers
+print(a/b)
